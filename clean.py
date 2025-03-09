6a0768c85ee4815f8d74a0cf3a9903f5187f58e8
TLouf <loufthomas@gmail.com>
2025-03-09 17:39:57 +0100

    clarify file names for students

--- clean.py
+++ clean.py
@@ -7,15 +7,15 @@ from tqdm import tqdm
 from traitlets.config import Config
 
 
-def clean(path, config, suffix="-sol"):
+def clean(path, config, prefix="sol-"):
     exporter = NotebookExporter(config=config)
     exporter.register_preprocessor(TagRemovePreprocessor(config=config), True)
 
     # Configure and run our exporter - returns a tuple - first element with ipynb,
     # second with notebook metadata
-    for p in tqdm(Path(path).glob(f"*/*{suffix}.ipynb")):
+    for p in tqdm(Path(path).glob(f"*/{prefix}*.ipynb")):
         output = exporter.from_filename(str(p))
-        p.with_stem(p.stem.removesuffix(suffix)).write_text(output[0])
+        p.with_stem(p.stem.removeprefix(prefix)).write_text(output[0])
 
 
 if __name__ == "__main__":
